1f051add8c9d225e9a4bff79c5833f31d1e35a21
unknown <you@example.com>
2020-10-17 09:12:10 +0600

    xyz]

---
+++ src/javaapplication44/JavaApplication44.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication44;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sayel
+ */
+public class JavaApplication44 {
+public static void main(String[] args) {
+ 
+    
+        
+      System.out.print("    1\n");
+      System.out.print("   1    1\n");
+      System.out.print("  1   2    1\n");
+      System.out.print("1   3    3    1\n");
+      
+}
+       
+}
